405282aa0df212a0cd25b7005db471ec41b556df
Pomsky2011 <104283528+Pomsky2011@users.noreply.github.com>
2022-12-26 13:18:48 -0800

    Add more personality to stevil :)

--- test.py
+++ test.py
@@ -46,7 +46,7 @@ while(1):
 			SpeakText(MyText)
 			
 	except sr.RequestError as e:
-		print("Could not request results; {0}".format(e))
+		print("Could not request results; {0}. F*** YOU!".format(e))
 		
 	except sr.UnknownValueError:
-		print("unknown error occurred")
+		print("Unknown error occurred? Little piece of s***")
